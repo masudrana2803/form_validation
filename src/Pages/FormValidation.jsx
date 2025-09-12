790e5c2e3d6c247fc117ac7f406cbc566e9d2ad9
masudrana2803 <masudrana2803@gmail.com>
2025-09-12 14:43:23 +0600

    Done

---
+++ src/Pages/FormValidation.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const FormValidation = () => {
+  return (
+    <>
+    <section className='bg-red-100 h-[100vh] flex justify-center items-center'>
+        <form className='w-[557px] bg-amber-500 p-25'>
+<div className='bg-red-800 text-center p-10 flex justify-between'>
+    <div className='bg-blue-100 p-5'>I am part 1</div>
+    <div className='bg-blue-200 p-5'>I am part 2</div>
+    <div className='bg-blue-300 p-5'>I am part 3</div>
+</div>
+<div className='bg-red-200 text-center p-10'>I am one</div>
+<div className='bg-red-300 text-center p-10'>I am one</div>
+<div className='bg-red-400 text-center p-10'>I am one</div>
+
+
+
+
+        </form>
+    </section>
+    </>
+  )
+}
+
+export default FormValidation
